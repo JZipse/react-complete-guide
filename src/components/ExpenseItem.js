d3c369dcaf813bc69bba364f886d0125b0bef6ca
JD Zipse <jzipse33@gmail.com>
2022-03-15 13:40:14 -0500

    Updated Component format

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,17 +1,17 @@
 import "./ExpenseItem.css";
 import ExpenseDate from "./ExpenseDate";
+import Card from "./Card"
 
 function ExpenseItem({ title, amount, date }) {
     
-
   return (
-    <div className="expense-item">
+    <Card className="expense-item">
     <ExpenseDate date={date} /> 
       <div>
         <h2 className="expense-item__description">{title}</h2>
       </div>
       <div className="expense-item__price">${amount}</div>
-    </div>
+    </Card>
   );
 }
 
